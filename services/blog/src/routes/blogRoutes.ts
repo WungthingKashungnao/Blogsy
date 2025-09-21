e5f497323ccc590a38564a3a59bcf8ed962cf0b2
wungthing <wungthingkashungnao123@gmail.com>
2025-09-22 00:11:24 +0530

    file cleanup

--- services/blog/src/routes/blogRoutes.ts
+++ services/blog/src/routes/blogRoutes.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import { getAllBlogs } from "../controllers/blogController.js";
-import { isAuth } from "../middleware/isAuth.js";
 
 const router = express.Router();
 
