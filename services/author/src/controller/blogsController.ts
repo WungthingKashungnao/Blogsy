21b7418889ce687509873062425176f23c7b315b
wungthing <wungthingkashungnao123@gmail.com>
2025-09-21 21:53:49 +0530

    controller added to update blog from author

--- services/author/src/controller/blogsController.ts
+++ services/author/src/controller/blogsController.ts
@@ -7,14 +7,14 @@ import cloudinary from "cloudinary";
 export const createBlog = TryCatch(async (req: AuthenticatedRequest, res) => {
   const { title, description, blogcontent, category } = req.body;
 
-  const file = req.file;
+  const file = req.file; //recieving file from user which is in binary data format because of multer
   if (!file) {
     return res.status(400).json({
       message: "No file to upload",
     });
   }
 
-  const fileBuffer = getBuffer(file);
+  const fileBuffer = getBuffer(file); //take the file in binary format transforming to data uri format
 
   if (!fileBuffer || !fileBuffer.content) {
     return res.status(400).json({
@@ -36,5 +36,61 @@ export const createBlog = TryCatch(async (req: AuthenticatedRequest, res) => {
   return res.status(200).json({
     message: "Blog Created Successfully",
     blogs: result[0],
+  });
+});
+
+export const updateBlog = TryCatch(async (req: AuthenticatedRequest, res) => {
+  const { id } = req.params;
+  const { title, description, blogcontent, category } = req.body;
+
+  const file = req.file;
+
+  const blog: any = await sql`SELECT * FROM blogs WHERE id = ${id}`;
+
+  if (!blog.length) {
+    return res.status(404).json({
+      message: "No Blog with this ids",
+    });
+  }
+
+  if (blog[0].author !== req.user?._id) {
+    return res.status(401).json({
+      message: "You are not the author of this blog",
+    });
+  }
+
+  let imageUrl = blog[0].image;
+
+  if (file) {
+    const fileBuffer = getBuffer(file); //take the file in binary format transforming to data uri format
+
+    if (!fileBuffer || !fileBuffer.content) {
+      return res.status(400).json({
+        message: "Failed to generate buffer",
+      });
+    }
+
+    //   uploading the file to cloudinary in a folder called blogs
+    const cloud = await cloudinary.v2.uploader.upload(fileBuffer.content, {
+      folder: "blogs",
+    });
+
+    imageUrl = cloud.secure_url;
+  }
+
+  const updateBlog = await sql`
+    UPDATE blogs SET
+      title = ${title || blog[0].title},
+      description = ${description || blog[0].description},
+      image = ${imageUrl},
+      blogcontent = ${blogcontent || blog[0].blogcontent},
+      category = ${category || blog[0].category}
+    WHERE id = ${id}
+    RETURNING *
+  `;
+
+  return res.status(200).json({
+    message: "Blog Updated",
+    blog: updateBlog[0],
   });
 });
--- services/author/src/routes/blogRoutes.ts
+++ services/author/src/routes/blogRoutes.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { isAuth } from "../middleware/isAuth.js";
 import uploadFile from "../middleware/multer.js";
-import { createBlog } from "../controller/blogsController.js";
+import { createBlog, updateBlog } from "../controller/blogsController.js";
 
 const router = express();
 
 router.post("/blog/new", isAuth, uploadFile, createBlog);
+router.put("/blog/:id", isAuth, uploadFile, updateBlog);
 
 export default router;
