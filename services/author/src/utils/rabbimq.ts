5f4eaaf4c84df8a5f0865b8cc6ec1738995050a1
wungthing <wungthingkashungnao123@gmail.com>
2025-09-23 18:04:51 +0530

    rabbitmq function to publish and invalidate

--- services/author/src/utils/rabbimq.ts
+++ services/author/src/utils/rabbimq.ts
@@ -15,6 +15,37 @@ export const connectRabbitMQ = async () => {
     channel = await connection.createChannel();
     console.log("✅ Connected To Rabbitmq");
   } catch (error) {
-    console.log("❌ Failed To Connect To Rabbitmq");
+    console.error("❌ Failed To Connect To Rabbitmq", error);
+  }
+};
+
+// function to publish message to queue
+export const publishToQueue = async (queueName: string, message: any) => {
+  if (!channel) {
+    console.log("Rabbitmq channel is not initialized");
+    return;
+  }
+
+  // Declaring a Queue
+  await channel.assertQueue(queueName, { durable: true }); // durable: true makes the queue persistent
+
+  // sendin message to queue
+  channel.sendToQueue(queueName, Buffer.from(JSON.stringify(message)), {
+    persistent: true,
+  });
+};
+
+// function to invalidate cache
+export const invalidateCacheJob = async (cacheKeys: string[]) => {
+  try {
+    const message = {
+      action: "invalidateCache",
+      keys: cacheKeys,
+    };
+
+    await publishToQueue("cache-invalidation", message);
+    console.log("✅ Cache Invalidation job published to Rabbitmq");
+  } catch (error) {
+    console.error("❌ Failed To Publish Cache on Rabbitmq", error);
   }
 };
