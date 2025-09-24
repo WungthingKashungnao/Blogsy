3a44d61673470575f463e4179ee292248af0f72b
wungthing <wungthingkashungnao123@gmail.com>
2025-09-25 00:00:42 +0530

    google api auth set up and front end

--- services/user/src/controllers/userControllers.ts
+++ services/user/src/controllers/userControllers.ts
@@ -4,16 +4,30 @@ import TryCatch from "../utils/TryCatch.js";
 import type { AuthenticatedRequest } from "../middleware/isAuth.js";
 import getBuffer from "../utils/dataUri.js";
 import { v2 as cloudinary } from "cloudinary";
+import { oauth2client } from "../utils/GoogleConfig.js";
+import axios from "axios";
 
 export const loginUser = TryCatch(async (req, res) => {
-  const { email, name, image } = req.body;
+  const { code } = req.body;
+  if (!code) {
+    return res.status(400).json({
+      message: "Authorization code is required",
+    });
+  }
+  const googleRes = await oauth2client.getToken(code);
+  oauth2client.setCredentials(googleRes.tokens);
+
+  const userRes = await axios.get(
+    `https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token =${googleRes.tokens.access_token}`
+  );
+  const { email, name, picture } = userRes.data;
   let user = await User.findOne({ email });
 
   if (!user) {
     user = await User.create({
       name,
       email,
-      image,
+      picture,
     });
   }
 
