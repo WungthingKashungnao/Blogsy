27dc003304a4e5fd7d621b31917e9fe2c892467f
wungthing <wungthingkashungnao123@gmail.com>
2025-09-25 17:54:20 +0530

    app context set up for user and login

--- frontend/src/app/context/AppContext.tsx
+++ frontend/src/app/context/AppContext.tsx
@@ -1,5 +1,84 @@
 "use client";
 
+import React, {
+  ReactNode,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import Cookies from "js-cookie";
+import axios from "axios";
+
 export const user_service = "http://localhost:5000";
 export const author_service = "http://localhost:5001";
 export const blog_service = "http://localhost:5002";
+
+export interface User {
+  _id: string;
+  name: string;
+  email: string;
+  image: string;
+  instagram: string;
+  facebook: string;
+  linkedin: string;
+  bio: string;
+}
+
+export interface blog {
+  id: string;
+  title: string;
+  description: string;
+  blogcontent: string;
+  image: string;
+  category: string;
+  author: string;
+  created_at: string;
+}
+
+interface AppContextType {
+  user: User | null;
+}
+
+const AppContext = createContext<AppContextType | undefined>(undefined);
+
+interface AppProviderProps {
+  children: ReactNode;
+}
+
+export const AppProvider: React.FC<AppProviderProps> = ({ children }) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [isAuth, setIsAuth] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  async function fetchUser() {
+    try {
+      const token = Cookies.get("token");
+      const { data } = await axios.get<User>(`${user_service}/api/v1/me`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setUser(data);
+      setIsAuth(true);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  return <AppContext.Provider value={{ user }}>{children}</AppContext.Provider>;
+};
+
+export const useAppData = (): AppContextType => {
+  const context = useContext(AppContext);
+  if (!context) {
+    throw new Error("useappdata must be used within AppProvider");
+  }
+  return context;
+};
